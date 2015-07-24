dd8c43998edee7b90c5c92cefa4e080972278c3d
fukai <fukai@jvche.cn>
2015-07-24 16:11:51 +0800

    ccj first build

---
+++ ccj_swl/ccj_swl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ccj_swl
+//
+//  Created by fukai on 15/7/24.
+//  Copyright (c) 2015年 swl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ccj_swl/ccj_swl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ccj_swl
+//
+//  Created by fukai on 15/7/24.
+//  Copyright (c) 2015年 swl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
